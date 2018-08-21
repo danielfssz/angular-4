0ec8ad8868673b0e1221789655c76d91a93891f9
Daniel Souza <danielfss98@gmail.com>
2018-08-21 20:43:19 -0300

    primeiro commit

---
+++ src/app/topo/topo.component.ts
@@ -0,0 +1,15 @@
+import { Component } from "@angular/core";
+
+/**
+ * Decorator - informa metadados atraves de um json
+ */
+@Component({
+    selector:'app-topo',//rotulo para instancia
+    templateUrl:'./topo.component.html' //arquivo de view
+})
+
+
+
+export class TopoComponent {
+
+}
