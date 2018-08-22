7b0cbedb39a2c6c886b549f5b6d78002a6582e6c
Daniel Souza <danielfss98@gmail.com>
2018-08-21 21:41:19 -0300

    Template implementado

--- src/app/topo/topo.component.ts
+++ src/app/topo/topo.component.ts
@@ -4,11 +4,10 @@ import { Component } from "@angular/core";
  * Decorator - informa metadados atraves de um json
  */
 @Component({
-    selector:'app-topo',//rotulo para instancia
-    templateUrl:'./topo.component.html' //arquivo de view
+    selector: 'app-topo',//rotulo para instancia
+    templateUrl: './topo.component.html', //arquivo de view
+    styleUrls: ['./topo.component.css']
 })
-
-
 
 export class TopoComponent {
 
